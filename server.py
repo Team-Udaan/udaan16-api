29fac18364fdc29ea073b41f19cdce4b6240ddca
alaypatel07 <alay1431@gmail.com>
2016-03-09 02:36:56 +0530

    Add src/subscribe/alert.py

--- server.py
+++ server.py
@@ -1,26 +1,19 @@
 import tornado
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from tornado.web import Application, StaticFileHandler
+from tornado.web import StaticFileHandler
 from tornado.options import define, options
 from src.test import TestHandler, TestMultipartHandler
-from src.subscribe.subscribe import SubscribeHander
-import motor
 
 define("port", default=8000, help="run on given port", type=int)
-
-client = motor.MotorClient()
 
 if __name__ == '__main__':
     options.parse_command_line()
     app = tornado.web.Application(handlers=[
         (r"/test", TestHandler),
         (r"/testmultipart", TestMultipartHandler),
-        (r"/subscribe", SubscribeHander),
         (r"/testmultipart/(.*)", StaticFileHandler, {"path": "images/"})
-    ],
-        client=client
-    )
+    ])
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
---
+++ src/subscribe/alert.py
@@ -0,0 +1,29 @@
+import requests
+from src.base import BaseHandler
+
+
+class AlertHandler(BaseHandler):
+
+    def send_mail(self, text, image, image_name):
+
+        return requests.post(
+            "https://api.mailgun.net/v3/sandbox1713f24a60034b5ab5e7fa0ca2faa9b6.mailgun.org/messages",
+            auth=("api", "key-a0bd92feef0ccecb07f199b770449917"),
+            files=[("inline", image)],
+            data={
+                "from": "Mailgun Sandbox <postmaster@sandbox1713f24a60034b5ab5e7fa0ca2faa9b6.mailgun.org>",
+                "to": "Udaan <udaan16developers@gmail.com>",
+                "subject": "Hello Udaan",
+                "text": text,
+                "html": '<html>Inline image here: <img src="cid:' + image_name + '"></html>'
+                }
+            )
+
+    def post(self, *args, **kwargs):
+        # TODO
+        # save the messages to local database
+        file = self.request.files['image'][0]
+        file_name = file["filename"]
+        image = file['body']
+        text = self.get_argument("text")
+        print(self.send_mail(text, image, file_name))
