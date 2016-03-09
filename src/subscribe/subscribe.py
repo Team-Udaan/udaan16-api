abbc527cb1dae4c1bfc0f50df40fb9122c1799b0
sandeepanjara <anjaras473@gmail.com>
2016-03-09 08:13:22 +0530

    Update /src/subscribe/subscribe.py /src/subscribe/alert.py

--- src/subscribe/subscribe.py
+++ src/subscribe/subscribe.py
@@ -1,8 +1,18 @@
+import requests
 from tornado.gen import coroutine
 from src.base import BaseHandler
 
 
 class SubscribeHander(BaseHandler):
+
+    def add_list_member(self, email_id):
+        return requests.post(
+            "https://api.mailgun.net/v3/lists/LIST@sandbox1713f24a60034b5ab5e7fa0ca2faa9b6.mailgun.org/members",
+            auth=('api', 'key-a0bd92feef0ccecb07f199b770449917'),
+            data={'subscribed': True,
+                  'address': email_id,
+                  'description': 'Subscribers',
+                  })
 
     @coroutine
     def post(self, *args, **kwargs):
@@ -10,6 +20,7 @@ class SubscribeHander(BaseHandler):
         db = self.settings['client'].subscription
         result = yield db.subscribers.insert({'email': data['email']})
         if result:
+            self.add_list_member(data['email'])
             response = 'email-id successfully inserted'
             # TODO
             # create list of status codes in basehandlers.
@@ -19,6 +30,7 @@ class SubscribeHander(BaseHandler):
             response = 'email-id not inserted'
             status_code = 400
             self.respond(response, status_code)
+
 
 
 
