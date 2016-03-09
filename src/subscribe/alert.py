df5caaf06729afc5079ded381323e0739b48333d
sandeepanjara <anjaras473@gmail.com>
2016-03-09 13:02:18 +0530

    update /src/subscribe/alert.py

--- src/subscribe/alert.py
+++ src/subscribe/alert.py
@@ -11,7 +11,7 @@ class AlertHandler(BaseHandler):
             files=[("inline", image)],
             data={
                 "from": "Mailgun Sandbox <postmaster@sandbox1713f24a60034b5ab5e7fa0ca2faa9b6.mailgun.org>",
-                "to": "<list@sandbox1713f24a60034b5ab5e7fa0ca2faa9b6.mailgun.org    >",
+                "to": "<list@sandbox1713f24a60034b5ab5e7fa0ca2faa9b6.mailgun.org>",
                 "subject": "Hello Udaan",
                 "text": text,
                 "html": '<html>Inline image here: <img src="cid:' + image_name + '"></html>'
@@ -21,8 +21,18 @@ class AlertHandler(BaseHandler):
     def post(self, *args, **kwargs):
         # TODO
         # save the messages to local database
+
         file = self.request.files['image'][0]
         file_name = file["filename"]
         image = file['body']
         text = self.get_argument("text")
-        print(self.send_mail(text, image, file_name))
+        response = self.send_mail(text, image, file_name)
+        if response:
+            response = "email send successfully"
+            status_code = 200
+            self.respond(response,status_code)
+        else:
+            response = "Please try again"
+            status_code = 400
+            self.respond(response, status_code)
+
