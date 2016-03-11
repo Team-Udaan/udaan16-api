a78206b18d7f6c845decd14aefd01e37475a6fe9
alaypatel07 <alay1431@gmail.com>
2016-03-11 13:21:37 +0530

    udaan16-api-4 Alert Response

--- src/test.py
+++ src/test.py
@@ -31,16 +31,26 @@ class TestMultipartHandler(BaseHandler):
 
 class TestSMSDeliveryHandler(BaseHandler):
 
-    def get(self, *args, **kwargs):
-        response = {}
-        data = self._get_arguments("data", self.request.arguments)
-        response["data"] = json.loads(data[0])
-        response["headers"] = {}
-        for header in self.request.headers.get_all():
-            temp = dict()
-            temp[header[0]] = header[1]
-            response['headers'].update(temp)
+    def serve(self):
+        print(self.request)
+        with open("record.txt", "a+") as f:
+            f.write(str(self.request) + "\n")
+        self.respond("working", 200)
 
-        response["body"] = self.request.body.decode()
-        print(response)
-        self.respond(response, 200)
+    def get(self, *args, **kwargs):
+        self.serve()
+        # response = {}
+        # data = self._get_arguments("data", self.request.arguments)
+        # response["data"] = json.loads(data[0])
+        # response["headers"] = {}
+        # for header in self.request.headers.get_all():
+        #     temp = dict()
+        #     temp[header[0]] = header[1]
+        #     response['headers'].update(temp)
+        #
+        # response["body"] = self.request.body.decode()
+        # print(response)
+        # self.respond(response, 200)
+
+    def post(self, *args, **kwargs):
+        self.serve()
