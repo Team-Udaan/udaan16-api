cadaecbdbd190b309457dc925e5ca260fd4f5c2a
sandeepanjara <anjaras473@gmail.com>
2016-03-18 12:48:41 +0530

    Add src/event_management/participants.py Update src/sms/sendsms.py

---
+++ src/event_management/participants.py
@@ -0,0 +1,63 @@
+from urllib import parse
+from urllib.parse import parse_qsl
+
+from bson import ObjectId
+
+from src.base import BaseHandler
+
+
+class ParticipantsHandler(BaseHandler):
+    def get(self, *args, **kwargs):
+        db = self.settings['client'].udaan
+        url = self.request.uri
+        unquoted_url = parse.unquote(url)
+        parsed_url = parse.urlparse(unquoted_url)
+        parameters = parse_qsl(parsed_url.query)
+        parameters = dict(parameters)
+        token = parameters['token']
+        round_number = parameters['round']
+        token = ObjectId(token)
+        result = yield db.eventCollection.find_one({"_id": token})
+        message = list()
+        if result:
+            if round_number == "current":
+                participants = db.paricipants.find({"round" + result['currentRound']: "q"},
+                                                   {"_id": 1, "names": 1, "mobileNumber": 1})
+                while (yield participants.fetch_next):
+                    document = participants.next_object()
+                    message.append(document)
+                self.respond(message, 200)
+            else:
+                participants = db.paricipants.find({"round" + round_number: "q"},
+                                                   {"_id": 1, "names": 1, "mobileNumber": 1})
+                while (yield participants.fetch_next):
+                    document = participants.next_object()
+                    message.append(document)
+                self.respond(message, 200)
+        else:
+            self.respond("token invalid", 401)
+
+    def post(self, *args, **kwargs):
+        data = self.get_request_body()
+        db = self.settings['client'].udaan
+        username = data['username']
+        mobile_number = data['mobileNumber']
+        token = data['token']
+        token = ObjectId(token)
+        result = yield db.eventCollection.find_one({"_id": token})
+        if result:
+            document = dict(
+                    name=username,
+                    mobileNumber=mobile_number,
+                    round=result['currentRound'],
+                    round1="q",
+                    round2="q",
+                    round3="n/a",
+            )
+            insert_result = db.paricipants.insert(document)
+            if insert_result:
+                self.respond(insert_result.__str__(), 200)
+            else:
+                self.respond("enable to insert", 400)
+        else:
+            self.respond("token invalid", 401)
--- src/sms/sendsms.py
+++ src/sms/sendsms.py
@@ -1,8 +1,10 @@
 import json
 from urllib import parse
+
 from bson import ObjectId
 from tornado.gen import coroutine
 from tornado.httpclient import AsyncHTTPClient
+
 from src.base import BaseHandler
 
 
@@ -20,45 +22,63 @@ class SendSMS(BaseHandler):
            else a reponse with status code 400 is sent.
            :param args:
            :param kwargs: """
-        round_number = ""
-        event_name = ""
-        date = ""
-        time = ""
-        venue = ""
+        db = self.settings['client'].udaan
         data = self.get_request_body()
-        db = self.settings['client'].sms
-        msg = data['message']
-        numbers = ','.join(map(str, data['numbers']))
-        test = data["test"]
-        sms_id = ObjectId()
-        data = {
-            "username": BaseHandler.environmental_variables["TEXTLOCAL_USERNAME"],
-            "hash": BaseHandler.environmental_variables["TEXTLOCAL_HASH"],
-            "numbers": numbers,
-            "message": "Dear Participant, Round " + round_number + " of " + event_name +
-                       "is on " + date + " " + time +
-                       " at " + venue + ".Kindly be present at the venue on time.",
-            "sender": BaseHandler.environmental_variables["TEXTLOCAL_SENDER"],
-            "custom": str(sms_id),
-            'test': test
-        }
-        request_data = parse.urlencode(data)
-        client = AsyncHTTPClient()
-        response = yield client.fetch("http://api.textlocal.in/send/", method='POST', headers=None, body=request_data)
-        response = json.loads(response.body.decode())
-        print(response, data)
-        if response["status"] == "success":
-            document = dict(
-                _id=sms_id,
-                numbers=numbers,
-                message=msg,
-                test=test
-            )
-            try:
-                yield db.smsCollection.insert(document)
-                self.respond(sms_id.__str__(), 200)
-            except Exception as e:
-                self.respond(e.__str__(), 500)
+        token = data['token']
+        teams = data['teams']
+        message = data['message']
+        date = data['date']
+        time = data['time']
+        venue = data['venue']
+        test = data['test']
+        token = ObjectId(token)
+        result = yield db.eventCollection.find_one({"_id": token})
+        update = yield db.eventCollection.update({"_id": token}, {"$inc": {"currentRound": 1}})
+        numbers = list()
+        if result:
+            for team in teams:
+                participants_result = yield db.participants.update({"mobileNumber": team['mobileNumber']},
+                                                                   {"$set": {"round": result['currentRound'],
+                                                                             "round" + result['currentRound']: "q"}}
+                                                                   )
+                numbers.append(participants_result['mobileNumber'])
+            round_number = result['currentRound']
+            event_name = result['event_name']
+            numbers = ','.join(map(str, data['numbers']))
+            db = self.settings['client'].sms
+            sms_id = ObjectId()
+            data = {
+                "username": BaseHandler.environmental_variables["TEXTLOCAL_USERNAME"],
+                "hash": BaseHandler.environmental_variables["TEXTLOCAL_HASH"],
+                "numbers": numbers,
+                "message": "Dear Participant, Round " + round_number + " of " + event_name +
+                           "is on " + date + " " + time +
+                           " at " + venue + ".Kindly be present at the venue on time.",
+                "sender": BaseHandler.environmental_variables["TEXTLOCAL_SENDER"],
+                "custom": str(sms_id),
+                'test': test
+            }
+            request_data = parse.urlencode(data)
+            client = AsyncHTTPClient()
+            response = yield client.fetch("http://api.textlocal.in/send/", method='POST', headers=None,
+                                          body=request_data)
+            response = json.loads(response.body.decode())
+            print(response, data)
+            if response["status"] == "success":
+                document = dict(
+                        _id=sms_id,
+                        numbers=numbers,
+                        message=message,
+                        test=test
+                )
+                try:
+                    yield db.smsCollection.insert(document)
+                    self.respond(sms_id.__str__(), 200)
+                except Exception as e:
+                    self.respond(e.__str__(), 500)
+            else:
+                response = "unable to send"
+                self.respond(response, 400)
+
         else:
-            response = "unable to send"
-            self.respond(response, 400)
+            self.respond("token invalid", 401)
