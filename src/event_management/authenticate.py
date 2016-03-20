092bc06316ee5ab5c0f146bf909c480c74069c72
alaypatel07 <alay1431@gmail.com>
2016-03-20 18:20:04 +0530

    Update authenticate.py to handle no Authorization header

--- src/event_management/authenticate.py
+++ src/event_management/authenticate.py
@@ -10,14 +10,17 @@ def authenticate(function):
     @wraps(function)
     def wrapper(self, *args, **kwargs):
         token = self.request.headers.get("Authorization")
-        try:
-            _id = ObjectId(token)
-            result = yield self.db.events.find_one({"_id": _id})
-            self.set_result(result)
-            if self.result is not None:
-                yield function(self, *args, **kwargs)
-            else:
-                self.respond("Invalid Token", 401)
-        except InvalidId as i:
-            self.respond(str(i), 400)
+        if token is None:
+            self.respond("Forbidden", 403)
+        else:
+            try:
+                _id = ObjectId(token)
+                result = yield self.db.events.find_one({"_id": _id})
+                self.set_result(result)
+                if self.result is not None:
+                    yield function(self, *args, **kwargs)
+                else:
+                    self.respond("Invalid Token", 401)
+            except Exception as i:
+                self.respond(str(i), 400)
     return wrapper
